eaeb54c059670fdb21201d7564d6aabb10694c51
EddyBer <eddybernardeau@gmail.com>
2023-06-17 12:25:25 +0200

    #mineur
    Ajout de la suppression d'un client

--- src/vues/ClientsList.vue
+++ src/vues/ClientsList.vue
@@ -9,6 +9,9 @@ import { VDataTable } from 'vuetify/labs/VDataTable'
 const authStore = useAuthStore();
 const listStore = uselistStore();
 
+const dialogDelete = ref(false)
+const clientToDelete = ref({})
+
 defineComponent({
 
 })
@@ -21,11 +24,30 @@ const headers = [
     { title: 'phone', align:'center', key :'phone'},
     { title: 'email', align:'center', key :'email'},
     { title: 'SIRET', align:'center', key :'SIRET'},
+    {title : 'actions' , align:'center' , key:'actions'}
 ]
 
 onMounted(async() => {
     listStore.loadData('clients')
 })
+
+const closeDelete = () => {
+    dialogDelete.value = false
+}
+
+const deleteItem = (item) => {
+    clientToDelete.value = item
+    dialogDelete.value = true
+}
+
+const deleteItemConfirm = async () => {
+    const res = await clientsApi.delete(clientToDelete.value)
+    if (res > 0) {
+        closeDelete()
+    } else {
+        alert('Erreur lors de la supression')
+    }
+}
 
 const clientList = ref(listStore.clientData)
 </script>
@@ -46,5 +68,23 @@ const clientList = ref(listStore.clientData)
         class="elevation-1 h-100"
         :search="search"
     >
+    <template v-slot:item.actions="{ item }">
+        <v-icon
+        size="small"
+        class="me-2"
+        @click="deleteItem(item.raw)"
+      >mdi-delete</v-icon></template>
     </v-data-table>
+
+    <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this client?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 </template>
